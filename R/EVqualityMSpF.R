51ce921706edb331c7f46484df637e86663a73b5
ruma1974 <runem2009@gmail.com>
2024-01-11 10:54:44 +0000

    Updated code with new functions

--- R/EVqualityMSpF.R
+++ R/EVqualityMSpF.R
@@ -1,4 +1,31 @@
+# _"/media/rune/Jochime2/RscripterCool/Rprojects/rmpackges/EVqualityMSp.R"
 
+
+calculate_coverage_percentages <- function(sL1, sL2,caseSensitive=TRUE) {
+  if (length(sL1) == 0 || length(sL2) == 0) {
+    stop("Both vectors must contain elements.")
+  }
+  
+# Convert vectors to lowercase to make the comparison case-insensitive
+if (caseSensitive==FALSE){
+  sL1 <- tolower(sL1)
+  sL2 <- tolower(sL2)	
+}
+# Initialize an empty list to store coverage percentages for each term in vector1
+coverage_percentages <- list()
+# Loop through each term in vector1
+sLu=unique(sL2)
+for (term in sLu) {
+# Calculate the number of times the term appears in vector2
+term_count_in_vector1 <- sum(sL1 == term)
+term_count_in_vector2 <- sum(sL2 == term)
+# Calculate the percentage of coverage for the term
+coverage_percentage <- ( term_count_in_vector1/term_count_in_vector2) * 100
+# Store the coverage percentage in the list
+coverage_percentages[[term]] <- coverage_percentage
+}  
+return(coverage_percentages)
+}
 
 
 #' Quality control of EV data
@@ -111,6 +138,7 @@ qL=colSums(x[idxEL,-idx,drop=FALSE])/length(EVqualityMS::ExoData[["TopMarker"]])
 if (length(idxEL)==0){
 qL=0
 } 
+cat("Overall quality metrics:\n")
 print(qL)
 # calculate contamination and exosome markers
 if (length(idxCL)>0){
@@ -147,7 +175,7 @@ invisible(res)
 #' @examples
 #' ---
 #' @export
-heatmapEVqualityISEV <- function(DF,Fac=NULL,color = NULL,CatColL=NULL,IncludeAlbumine = TRUE,plotCatDescription=TRUE){
+heatmapEVqualityMISEV <- function(DF,Fac=NULL,color = NULL,CatColL=NULL,IncludeAlbumine = TRUE,plotCatDescription=TRUE){
 #
 #
 if (is.null(color)){color=c("grey60","grey30","gray15","steelblue3","navy","seagreen1","seagreen4","palegreen","violetred4","red")}
@@ -162,10 +190,11 @@ x=DF
 if (!is.null(Fac)){
 idx=which(names(DF)=="GN")
 mat=DF[,-idx]
-FUN = function(x) {median(x, na.rm = T)}
+FUN = function(x) {median(as.numeric(x), na.rm = T)}
 mat=t(apply(mat, 1, function(x) {tapply(x, Fac, FUN = FUN)}))
 x=as.data.frame(mat)
 x$GN=DF[,idx]
+print(x)
 }
 # rank scale data
 
@@ -228,8 +257,9 @@ colL[id2]="blue"
 clustL=rep("",nrow(mat))
 
 for (i in 1:nrow(mat)){
-print(rownames(mat)[i])
+#print(rownames(mat)[i])
 id=which(rownames(mat)[i]==EVqualityMS::DFanno$GN)
+#print(id)
 if (id>=0){
 
 if (plotCatDescription==TRUE){
@@ -239,10 +269,14 @@ clustL[i]=EVqualityMS::DFanno$Cat[id]
 }
 }
 }
-Ncol=clustL %>% unique %>% length
+#
+#print(clustL)
+#
+
+Ncol=length(unique(clustL))
 CatColL=CatColL[1:Ncol]
 names(CatColL)=unique(clustL)
-print(CatColL)
+#print(CatColL)
 p1 = ComplexHeatmap::Heatmap(t(mat), name = "mat", column_names_gp = ggfun::gpar(col = colL),cluster_columns = ComplexHeatmap::cluster_within_group(t(mat),as.numeric(factor(clustL))),heatmap_legend_param = list(position = "top", title = "Regulation", legend_direction = "horizontal"),top_annotation = ComplexHeatmap::HeatmapAnnotation(Category = clustL, col = list(Category = CatColL)))
 p1 = ComplexHeatmap::draw(p1, heatmap_legend_side = "bottom")
 
@@ -257,6 +291,7 @@ qL=colSums(x[idxEL,-idx,drop=FALSE])/length(EVqualityMS::ExoData[["TopMarker"]])
 if (length(idxEL)==0){
 qL=0
 } 
+cat("Overall quality metrics:\n")
 print(qL)
 # calculate contamination and exosome markers
 if (length(idxCL)>0){
@@ -281,18 +316,21 @@ invisible(res)
 #' @param Name (string - name of the new samples)
 #' @param src (string - which reference samples to include)
 #' @param palette (colorL or palette)
+#' @param jitter_b (boolean)
+#' @param Psize (float)
 #' @return ggplot
 #' @keywords Quality_control
 #' @examples
 #' ---
 #' @export
-scatterPlot <- function(x=NULL,Name="new",src="all",palette="jco"){
+scatterPlot <- function(x=NULL,Name="new",src="all",palette="jco",jitter_b=TRUE,Psize=3){
 #
 #
 #x = DFurine; Name = "Urine"; src = "plasma"
 if (!is.null(x)){
 res=EVqualityMS::HeatmapEVmarkers(x,plotHeat =FALSE)
 DF=data.frame(EVmarker=res$"EV marker",Contaminants=res$Contaminants,source=Name,sample="")
+print(DF)
 }
 DFref=EVqualityMS::DFqualityRef
 # select reference samples
@@ -309,13 +347,323 @@ DFref=rbind(DFref,DF)
 ## xplot <- xplot + ggpubr::clean_theme() + ggpubr::rremove("legend")
 # Arranging the plot using cowplot
 ## p <- cowplot::plot_grid(xplot, sp,ncol = 1, align = "hv", rel_heights = c(1, 1.5,1))
+if (jitter_b==TRUE){
+H=0.01
+W=0.01	
+} else {
+H=0
+W=0	
+}
+
+
 p=ggpubr::ggscatterhist(
-  DFref, x = "EVmarker", y = "Contaminants",position=ggplot2::position_jitter(h=0.05,w=0.05),
-  color = "source", size = "source",shape ="source", alpha = 0.6,font.tickslab = c(20, "plain"),font.legend = c(16, "plain"),font.x=c(16, "plain"),font.y=c(16, "plain"),
+  DFref, x = "EVmarker", y = "Contaminants",position=ggplot2::position_jitter(h=H,w=W),
+  color = "source", size = Psize,shape ="source", alpha = 0.6,font.tickslab = c(20, "plain"),font.legend = c(16, "plain"),font.x=c(16, "plain"),font.y=c(16, "plain"),
   palette = palette,
   margin.params = list(fill = "source", color = "black", size = 0.2)
   ) 
-
+#p=p+  ggplot2::geom_point(data = DF, ggplot2::aes(x = "EVmarker", y ="Contaminants"), color = "red", size = 3)
 return(p) # invisible(res)
 }
+
+
+
+#' Plot heatmap withsubpopulation  markers from  Kowal et al 2016 paper
+#' @param DF (DF - data frame with a GN column)
+#' @param Fac (Fac - should have length ncol(DF)-1)
+#' @param color (color - vector with ten colors for type of marker)
+#' @param CatColL (color - protein category)
+#' @param IncludeAlbumine (bool )
+#' @return list
+#' @keywords Quality_control
+#' @examples
+#' ---
+#' @export
+heatmapEVsub <- function(DF,Fac=NULL,color = NULL,CatColL=NULL,IncludeAlbumine = TRUE){
+#
+#
+if (is.null(color)){color=c("grey60","grey30","gray15","steelblue3","navy","seagreen1","seagreen4","palegreen","violetred4","red")}
+if (is.null(CatColL)){CatColL=c("#F0A3FF", "#0075DC", "#993F00", "#4C005C", "#191919", "#005C31", 
+"#2BCE48", "#FFCC99", "#808080", "#94FFB5", "#8F7C00", "#9DCC00", 
+"#C20088", "#003380", "#FFA405")}
+
+if (!is.data.frame(DF)){stop("x must be a data frame with a column named GN")}
+#
+x=DF
+# median of sample groups
+if (!is.null(Fac)){
+idx=which(names(DF)=="GN")
+mat=DF[,-idx]
+FUN = function(x) {median(as.numeric(x), na.rm = T)}
+mat=t(apply(mat, 1, function(x) {tapply(x, Fac, FUN = FUN)}))
+x=as.data.frame(mat)
+x$GN=DF[,idx]
+print(x)
+}
+# rank scale data
+
+for (i in 1:ncol(x)){
+if (names(x)[i]!="GN"){
+xL=as.numeric(x[,i])
+xL=scales::rescale(xL,to=c(0,1))
+xL[is.na(xL)]=0
+xL=rank(xL)/max(rank(xL))
+x[,i]=xL
+}
+}
+# remove duplicated GN
+idx=which(names(x) %in% "GN")
+Rsum=rowSums(as.matrix(x[,-idx]))
+idxL=order(Rsum,decreasing =TRUE)
+#Rsum[idxL]
+x=x[idxL,]
+x=x[!duplicated(x$GN),]
+
+#str(as.matrix(x[,-idx]))
+
+# merge with markers data
+# DF=EVqualityMS::ExoData[["Expression"]]
+#which(DFanno$GN=="ENO1")
+
+DF=data.frame(GN=EVqualityMS::DFsubEV$GN)
+DF=DF %>% dplyr::left_join( x, by = "GN")
+
+# remove NAs
+DF[is.na(DF)]=0
+mat=DF[,2:ncol(DF)]
+rownames(mat)=DF$GN
+# remove zero rows
+idxL=which(rowSums(mat)==0)
+if (length(idxL)>0){
+mat=mat[-idxL,]	
+}
+# IncludeAlbumine
+if (IncludeAlbumine==FALSE){mat=mat[rownames(mat)!="ALB",]}
+# plot
+colL=rep("black",nrow(mat))
+# Contaminants
+#conL=append(c("APOA1","APOA2","APOB","UMOD"),EVqualityMS::ExoData$Contaminants)
+#for (i in 1:length(conL)){
+#id2=which(conL[i]==rownames(mat))
+#colL[id2]="red"
+#}
+# TopMarker
+#for (i in 1:length(EVqualityMS::ExoData$TopMarker)){
+#id2=which(EVqualityMS::ExoData$TopMarker[i]==rownames(mat))
+#colL[id2]="green"
+#}
+# microvesicles
+#for (i in 1:length(EVqualityMS::ExoData$lEV)){
+#id2=which(EVqualityMS::ExoData$lEV[i]==rownames(mat))
+#colL[id2]="blue"
+#}
+# cluster
+clustL=rep("",nrow(mat))
+
+for (i in 1:nrow(mat)){
+#print(rownames(mat)[i])
+id=which(rownames(mat)[i]==EVqualityMS::DFsubEV$GN)
+#print(id)
+if (id>=0){
+clustL[i]=EVqualityMS::DFsubEV$Group[id]
+} # if id>0
+} # i
+#
+#print(clustL)
+percentage_coverages=calculate_coverage_percentages(clustL,EVqualityMS::DFsubEV$Group)
+cat("Percentage marker coverages:\n")
+for (term in names(percentage_coverages)) {
+  cat(term, ": ", percentage_coverages[[term]], "%\n")
+}
+#
+
+Ncol=length(unique(clustL))
+CatColL=CatColL[1:Ncol]
+names(CatColL)=unique(clustL)
+#print(CatColL)
+p1 = ComplexHeatmap::Heatmap(t(mat), name = "mat", column_names_gp = ggfun::gpar(col = colL),cluster_columns = ComplexHeatmap::cluster_within_group(t(mat),as.numeric(factor(clustL))),heatmap_legend_param = list(position = "top", title = "Scaled expression", legend_direction = "horizontal"),top_annotation = ComplexHeatmap::HeatmapAnnotation(Category = clustL, col = list(Category = CatColL)))
+p1 = ComplexHeatmap::draw(p1, heatmap_legend_side = "bottom")
+
+# print Q index 
+idxCL=which(x$GN %in% EVqualityMS::ExoData[["Contaminants"]])
+idxEL=which(x$GN %in% EVqualityMS::ExoData[["TopMarker"]])
+if (length(idxCL)>0){
+qL=colSums(x[idxEL,-idx,drop=FALSE])/length(EVqualityMS::ExoData[["TopMarker"]])-colSums(x[idxCL,-idx,drop=FALSE])/length(EVqualityMS::ExoData[["Contaminants"]])
+} else {
+qL=colSums(x[idxEL,-idx,drop=FALSE])/length(EVqualityMS::ExoData[["TopMarker"]])
+}
+if (length(idxEL)==0){
+qL=0
+} 
+cat("Overall quality metrics:\n")
+print(qL)
+# calculate contamination and exosome markers
+if (length(idxCL)>0){
+qC=colSums(x[idxCL,-idx,drop=FALSE])/length(EVqualityMS::ExoData[["Contaminants"]])
+} else {qC=0}
+if (length(idxEL)>0){
+qE=colSums(x[idxEL,-idx,drop=FALSE])/length(EVqualityMS::ExoData[["TopMarker"]])
+} else {qE=0}
+# collect results
+res=list()
+res[["Expression"]]=mat
+res[["Quality"]]=qL 
+res[["Contaminants"]]=qC
+res[["EV marker"]]=qE
+res[["Heatmap"]]=p1
+invisible(res)
+}
+
+
+
+#' Plot heatmap with subpopulation  markers from cd9, cd63 and CD81 pulldown from Kowal et al 2016 paper
+#' @param DF (DF - data frame with a GN column)
+#' @param Fac (Fac - should have length ncol(DF)-1)
+#' @param color (color - vector with ten colors for type of marker)
+#' @param CatColL (color - protein category)
+#' @param IncludeAlbumine (bool )
+#' @return list
+#' @keywords Quality_control
+#' @examples
+#' ---
+#' @export
+heatmapEVsubCD <- function(DF,Fac=NULL,color = NULL,CatColL=NULL,IncludeAlbumine = TRUE){
+#
+#
+if (is.null(color)){color=c("grey60","grey30","gray15","steelblue3","navy","seagreen1","seagreen4","palegreen","violetred4","red")}
+if (is.null(CatColL)){CatColL=c("#F0A3FF", "#0075DC", "#993F00", "#4C005C", "#191919", "#005C31", 
+"#2BCE48", "#FFCC99", "#808080", "#94FFB5", "#8F7C00", "#9DCC00", 
+"#C20088", "#003380", "#FFA405")}
+
+if (!is.data.frame(DF)){stop("x must be a data frame with a column named GN")}
+#
+x=DF
+# median of sample groups
+if (!is.null(Fac)){
+idx=which(names(DF)=="GN")
+mat=DF[,-idx]
+FUN = function(x) {median(as.numeric(x), na.rm = T)}
+mat=t(apply(mat, 1, function(x) {tapply(x, Fac, FUN = FUN)}))
+x=as.data.frame(mat)
+x$GN=DF[,idx]
+print(x)
+}
+# rank scale data
+
+for (i in 1:ncol(x)){
+if (names(x)[i]!="GN"){
+xL=as.numeric(x[,i])
+xL=scales::rescale(xL,to=c(0,1))
+xL[is.na(xL)]=0
+xL=rank(xL)/max(rank(xL))
+x[,i]=xL
+}
+}
+# remove duplicated GN
+idx=which(names(x) %in% "GN")
+Rsum=rowSums(as.matrix(x[,-idx]))
+idxL=order(Rsum,decreasing =TRUE)
+#Rsum[idxL]
+x=x[idxL,]
+x=x[!duplicated(x$GN),]
+
+#str(as.matrix(x[,-idx]))
+
+# merge with markers data
+# DF=EVqualityMS::ExoData[["Expression"]]
+#which(DFanno$GN=="ENO1")
+
+DF=data.frame(GN=EVqualityMS::DFsubCD$GN)
+DF=DF %>% dplyr::left_join( x, by = "GN")
+
+# remove NAs
+DF[is.na(DF)]=0
+mat=DF[,2:ncol(DF)]
+rownames(mat)=DF$GN
+# remove zero rows
+idxL=which(rowSums(mat)==0)
+if (length(idxL)>0){
+mat=mat[-idxL,]	
+}
+# IncludeAlbumine
+if (IncludeAlbumine==FALSE){mat=mat[rownames(mat)!="ALB",]}
+# plot
+colL=rep("black",nrow(mat))
+# Contaminants
+#conL=append(c("APOA1","APOA2","APOB","UMOD"),EVqualityMS::ExoData$Contaminants)
+#for (i in 1:length(conL)){
+#id2=which(conL[i]==rownames(mat))
+#colL[id2]="red"
+#}
+# TopMarker
+#for (i in 1:length(EVqualityMS::ExoData$TopMarker)){
+#id2=which(EVqualityMS::ExoData$TopMarker[i]==rownames(mat))
+#colL[id2]="green"
+#}
+# microvesicles
+#for (i in 1:length(EVqualityMS::ExoData$lEV)){
+#id2=which(EVqualityMS::ExoData$lEV[i]==rownames(mat))
+#colL[id2]="blue"
+#}
+# cluster
+clustL=rep("",nrow(mat))
+
+for (i in 1:nrow(mat)){
+#print(rownames(mat)[i])
+id=which(rownames(mat)[i]==EVqualityMS::DFsubCD$GN)
+#print(id)
+if (id>=0){
+clustL[i]=EVqualityMS::DFsubCD$Group[id]
+} # if id>0
+} # i
+#
+#print(clustL)
+# print percentage of markers identified
+#print(table(clustL))
+#print(table(EVqualityMS::DFsubCD$Group))
+# Print the coverage percentages for each term in vector1
+percentage_coverages=calculate_coverage_percentages(clustL,EVqualityMS::DFsubCD$Group)
+cat("Percentage marker coverages: \n")
+for (term in names(percentage_coverages)) {
+  cat(term, ": ", percentage_coverages[[term]], "%\n")
+}
+
+#
+Ncol=length(unique(clustL))
+CatColL=CatColL[1:Ncol]
+names(CatColL)=unique(clustL)
+#print(CatColL)
+p1 = ComplexHeatmap::Heatmap(t(mat), name = "mat", column_names_gp = ggfun::gpar(col = colL),cluster_columns = ComplexHeatmap::cluster_within_group(t(mat),as.numeric(factor(clustL))),heatmap_legend_param = list(position = "top", title = "Scaled expression", legend_direction = "horizontal"),top_annotation = ComplexHeatmap::HeatmapAnnotation(Category = clustL, col = list(Category = CatColL)))
+p1 = ComplexHeatmap::draw(p1, heatmap_legend_side = "bottom")
+
+# print Q index 
+idxCL=which(x$GN %in% EVqualityMS::ExoData[["Contaminants"]])
+idxEL=which(x$GN %in% EVqualityMS::ExoData[["TopMarker"]])
+if (length(idxCL)>0){
+qL=colSums(x[idxEL,-idx,drop=FALSE])/length(EVqualityMS::ExoData[["TopMarker"]])-colSums(x[idxCL,-idx,drop=FALSE])/length(EVqualityMS::ExoData[["Contaminants"]])
+} else {
+qL=colSums(x[idxEL,-idx,drop=FALSE])/length(EVqualityMS::ExoData[["TopMarker"]])
+}
+if (length(idxEL)==0){
+qL=0
+} 
+cat("Overall quality metrics:\n")
+print(qL)
+# calculate contamination and exosome markers
+if (length(idxCL)>0){
+qC=colSums(x[idxCL,-idx,drop=FALSE])/length(EVqualityMS::ExoData[["Contaminants"]])
+} else {qC=0}
+if (length(idxEL)>0){
+qE=colSums(x[idxEL,-idx,drop=FALSE])/length(EVqualityMS::ExoData[["TopMarker"]])
+} else {qE=0}
+# collect results
+res=list()
+res[["Expression"]]=mat
+res[["Quality"]]=qL 
+res[["Contaminants"]]=qC
+res[["EV marker"]]=qE
+res[["Heatmap"]]=p1
+invisible(res)
+}
+
 
