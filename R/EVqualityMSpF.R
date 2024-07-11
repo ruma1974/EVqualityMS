6372d9c9a21f9935575811312b769742e683eee7
ruma1974 <runem2009@gmail.com>
2024-07-11 12:26:03 +0100

    Updates files

--- R/EVqualityMSpF.R
+++ R/EVqualityMSpF.R
@@ -28,6 +28,36 @@ return(coverage_percentages)
 }
 
 
+
+#' Calculate summary stat from DF
+#' @param DF (DF - first column is values and second column is grouping vector)
+#' @return list
+#' @keywords stat
+#' @examples
+#' ---
+#' @export
+statFunc <- function(DF){
+#
+#
+names(DF)=c("numbers","groups")
+res <- doBy::summaryBy(numbers ~ groups, data = DF, FUN = function(x) {
+  c(
+    mean = mean(x,na.rm=TRUE),
+    median = median(x,na.rm=TRUE),
+    IQR = IQR(x,na.rm=TRUE),
+    min = min(x,na.rm=TRUE),
+    max = max(x,na.rm=TRUE),
+    SD = sd(x,na.rm=TRUE)
+  )
+})
+
+return(res) # invisible(res)
+}
+
+
+
+
+
 #' Quality control of EV data
 #' @param x (data frame with a GN column)
 #' @param Fac (Fac - should have length ncol(DF)-1)
@@ -72,9 +102,14 @@ x$GN[res$index]=rownames(res)
 for (i in 1:ncol(x)){
 if (names(x)[i]!="GN"){
 xL=as.numeric(x[,i])
-xL=scales::rescale(xL,to=c(0,1))
 xL[is.na(xL)]=0
-xL=rank(xL)/max(rank(xL))
+xL=scales::rescale(xL,to=c(0,1))
+
+# Rank with ties receiving the same ranking
+xL <- rank(xL, ties.method = "average")
+# Scale ranks from 0 to 1
+xL=xL-min(xL)
+xL=xL/max(xL)
 x[,i]=xL
 }
 }
@@ -159,7 +194,7 @@ res[["Heatmap"]]=p
 invisible(res)
 }
 
-
+ 
 
 
 
@@ -194,27 +229,37 @@ FUN = function(x) {median(as.numeric(x), na.rm = T)}
 mat=t(apply(mat, 1, function(x) {tapply(x, Fac, FUN = FUN)}))
 x=as.data.frame(mat)
 x$GN=DF[,idx]
-print(x)
+#print(x)
+#tmp1=x
 }
 # rank scale data
 
 for (i in 1:ncol(x)){
 if (names(x)[i]!="GN"){
 xL=as.numeric(x[,i])
-xL=scales::rescale(xL,to=c(0,1))
 xL[is.na(xL)]=0
-xL=rank(xL)/max(rank(xL))
+xL=scales::rescale(xL,to=c(0,1))
+# Rank with ties receiving the same ranking
+xL <- rank(xL, ties.method = "average")
+# Scale ranks from 0 to 1
+#xL <- (xL - 1) / (length(xL) - 1)
+#print(xL)
+#print(min(xL))
+#print(max(xL))
+#print("---------")
+xL=xL-min(xL)
+xL=xL/max(xL)
 x[,i]=xL
 }
 }
-# remove duplicated GN
+#tmp2=x
+# remove duplicated GN keep the one with most intensity
 idx=which(names(x) %in% "GN")
 Rsum=rowSums(as.matrix(x[,-idx]))
 idxL=order(Rsum,decreasing =TRUE)
 #Rsum[idxL]
 x=x[idxL,]
 x=x[!duplicated(x$GN),]
-
 #str(as.matrix(x[,-idx]))
 
 # merge with markers data
@@ -226,6 +271,7 @@ DF=DF %>% dplyr::left_join( x, by = "GN")
 
 # remove NAs
 DF[is.na(DF)]=0
+# move gene names to last column
 mat=DF[,2:ncol(DF)]
 rownames(mat)=DF$GN
 # remove zero rows
@@ -233,7 +279,7 @@ idxL=which(rowSums(mat)==0)
 if (length(idxL)>0){
 mat=mat[-idxL,]	
 }
-# IncludeAlbumine
+# Include Albumine
 if (IncludeAlbumine==FALSE){mat=mat[rownames(mat)!="ALB",]}
 # plot
 colL=rep("black",nrow(mat))
@@ -277,7 +323,9 @@ Ncol=length(unique(clustL))
 CatColL=CatColL[1:Ncol]
 names(CatColL)=unique(clustL)
 #print(CatColL)
-p1 = ComplexHeatmap::Heatmap(t(mat), name = "mat", column_names_gp = ggfun::gpar(col = colL),cluster_columns = ComplexHeatmap::cluster_within_group(t(mat),as.numeric(factor(clustL))),heatmap_legend_param = list(position = "top", title = "Regulation", legend_direction = "horizontal"),top_annotation = ComplexHeatmap::HeatmapAnnotation(Category = clustL, col = list(Category = CatColL)))
+
+#
+p1 = ComplexHeatmap::Heatmap(t(mat), name = "mat",col = color, column_names_gp = ggfun::gpar(col = colL),cluster_columns = ComplexHeatmap::cluster_within_group(t(mat),as.numeric(factor(clustL))),heatmap_legend_param = list(position = "top", title = "Regulation", legend_direction = "horizontal"),top_annotation = ComplexHeatmap::HeatmapAnnotation(Category = clustL, col = list(Category = CatColL)))
 p1 = ComplexHeatmap::draw(p1, heatmap_legend_side = "bottom")
 
 # print Q index 
@@ -307,6 +355,8 @@ res[["Quality"]]=qL
 res[["Contaminants"]]=qC
 res[["EV marker"]]=qE
 res[["Heatmap"]]=p1
+#res[["tmp1"]]=tmp1
+#res[["tmp2"]]=tmp2
 invisible(res)
 }
 
@@ -314,22 +364,32 @@ invisible(res)
 #' plot scatter of relative contamination and EV marker abundance
 #' @param x (DF- quantitative values in columns and last column holds the gene names - GN)
 #' @param Name (string - name of the new samples)
+#' @param Title (string - plot title) 
 #' @param src (string - which reference samples to include)
 #' @param palette (colorL or palette)
 #' @param jitter_b (boolean)
 #' @param Psize (float)
+#' @param scaleData (boolean)
+#' @param NoDensity (boolean)
+#' @param addRefLines (boolean)
 #' @return ggplot
 #' @keywords Quality_control
 #' @examples
 #' ---
 #' @export
-scatterPlot <- function(x=NULL,Name="new",src="all",palette="jco",jitter_b=TRUE,Psize=3){
+scatterPlot <- function(x=NULL,Name="new",Title="",src="all",palette="jco",jitter_b=TRUE,Psize=3,scaleData=TRUE,NoDensity=TRUE,addRefLines=FALSE){
 #
 #
 #x = DFurine; Name = "Urine"; src = "plasma"
 if (!is.null(x)){
 res=EVqualityMS::HeatmapEVmarkers(x,plotHeat =FALSE)
+if (Name[1]==""){
+Name=names(res$Quality)
+Name=sapply(strsplit(Name, "_"), function(x) x[1])
+DF=data.frame(EVmarker=res$"EV marker",Contaminants=res$Contaminants,source=Name,sample="")	
+}else {
 DF=data.frame(EVmarker=res$"EV marker",Contaminants=res$Contaminants,source=Name,sample="")
+}
 print(DF)
 }
 DFref=EVqualityMS::DFqualityRef
@@ -341,7 +401,7 @@ DFref=rbind(DFref,DF)
 }
 #
 # palette = c("red", "green", "blue")
-## sp <- ggpubr::ggscatter(DFref, x =  "EVmarker", y ="Contaminants",color = "source", palette = palette,size = 3, alpha = 0.6)+ggpubr::border()
+## sp <- ggpubr::ggscatter(DFref, x =  "EVmarker", y ="Non-EV marker",color = "source", palette = palette,size = 3, alpha = 0.6)+ggpubr::border()
 # Marginal density plot of x (top panel) and y (right panel)
 ## xplot <- ggpubr::ggdensity(DFref, x="EVmarker", fill = "source",palette = palette)
 ## xplot <- xplot + ggpubr::clean_theme() + ggpubr::rremove("legend")
@@ -354,21 +414,70 @@ W=0.01
 H=0
 W=0	
 }
-
-
+#data.frame(c(0,1,0,1),c(0,1,1,0))
+if (scaleData==TRUE){
 p=ggpubr::ggscatterhist(
-  DFref, x = "EVmarker", y = "Contaminants",position=ggplot2::position_jitter(h=H,w=W),
+  DFref, x = "EVmarker", y = "Contaminants",position=ggplot2::position_jitter(h=H,w=W),title = Title,
   color = "source", size = Psize,shape ="source", alpha = 0.6,font.tickslab = c(20, "plain"),font.legend = c(16, "plain"),font.x=c(16, "plain"),font.y=c(16, "plain"),
   palette = palette,
   margin.params = list(fill = "source", color = "black", size = 0.2)
-  ) 
+  )
 #p=p+  ggplot2::geom_point(data = DF, ggplot2::aes(x = "EVmarker", y ="Contaminants"), color = "red", size = 3)
+} else {
+# Main plot
+p <-  ggplot2::ggplot(DFref, ggplot2::aes(x = EVmarker, y = Contaminants, color = source))+ggplot2::geom_point()+ggplot2::scale_color_manual(values=palette)+ ggplot2::xlim(0, 1)+ ggplot2::ylim(0, 1) +ggplot2::geom_point(size=3) # +ggplot2::xlab('EV marker') + ggplot2::ylab('Non-EV marker')
+#
+p = p + ggplot2::theme_bw()
+p = p + ggplot2::theme(axis.text.x = ggplot2::element_text(family = "Arial", face = "plain", colour = "black", size = 20, hjust = 0.5, vjust = 0.5, angle = 0))
+p = p + ggplot2::theme(axis.text.y = ggplot2::element_text(family = "Arial", face = "plain", colour = "black", size = 20, hjust = 0.5, vjust = 0.5, angle = 0))
+p = p + ggplot2::theme(axis.title.x = ggplot2::element_text(family = "Arial", face = "plain", colour = "black", size = 22, hjust = 0.5, vjust = 0.5))
+p = p + ggplot2::theme(axis.title.y = ggplot2::element_text(family = "Arial", face = "plain", colour = "black", size = 22, hjust = 0.5, vjust = 0.5))
+p= p + ggplot2::labs(title=Title,x ="EV marker", y = "Non-EV marker")
+#
+p=p+ggplot2::theme(legend.position="bottom")+ ggplot2::theme(legend.text=ggplot2::element_text(size=16))
+#
+if (length(unique(DFref[,c("source")]))<=4 & NoDensity==FALSE){
+# Marginal densities along x axis
+xdens <-  cowplot::axis_canvas(p, axis = "x")+ggplot2::geom_density(data = DFref, ggplot2::aes(x = EVmarker, fill = source),alpha = 0.7, linewidth = 0.2)+ggplot2::scale_color_manual(values=palette)
+#ggpubr::fill_palette("jco") #+ xlim(0, 1)
+# Marginal densities along y axis
+
+# Need to set coord_flip = TRUE, if you plan to use coord_flip()
+ydens <-  cowplot::axis_canvas(p, axis = "y", coord_flip = TRUE)+ggplot2::geom_density(data = DFref, ggplot2::aes(x = Contaminants, fill = source),alpha = 0.7, size = 0.2)+ggplot2::coord_flip()+ggplot2::scale_color_manual(values=palette)
+#ggpubr::fill_palette("jco") 
+
+p <- cowplot::insert_xaxis_grob(p, xdens, grid::unit(.2, "null"), position = "top")
+p <- cowplot::insert_yaxis_grob(p, ydens, grid::unit(.2, "null"), position = "right")
+p=cowplot::ggdraw(p)
+
+} 
+}
+if (addRefLines==TRUE){
+p=p+ggplot2::geom_hline(yintercept=0.16, linetype="dashed", color = "red")	
+p=p+ggplot2::geom_hline(yintercept=0.32, linetype="dashed", color = "red")	
+p=p+ggplot2::geom_vline(xintercept=0, linetype="dashed", color = "red")	
+p=p+ggplot2::geom_vline(xintercept=0.93, linetype="dashed", color = "red")	
+}
+
+
+
+# fix xlab and ylab
+#p=p+ggplot2::labs(x = "EV marker", y = "Non-EV marker") # xlab("EV marker") + ylab("Non-EV marker")
+# stat
+print("EV marker:")
+DF=data.frame(values=DFref$EVmarker,groups=DFref$source)
+print(EVqualityMS::statFunc(DF))
+
+print("Non-EV markers:")
+DF=data.frame(values=DFref$Contaminants,groups=DFref$source)
+print(EVqualityMS::statFunc(DF))
+
 return(p) # invisible(res)
 }
 
 
 
-#' Plot heatmap withsubpopulation  markers from  Kowal et al 2016 paper
+#' Plot heatmap with subpopulation  markers from  Kowal et al 2016 paper
 #' @param DF (DF - data frame with a GN column)
 #' @param Fac (Fac - should have length ncol(DF)-1)
 #' @param color (color - vector with ten colors for type of marker)
@@ -405,9 +514,13 @@ print(x)
 for (i in 1:ncol(x)){
 if (names(x)[i]!="GN"){
 xL=as.numeric(x[,i])
-xL=scales::rescale(xL,to=c(0,1))
 xL[is.na(xL)]=0
-xL=rank(xL)/max(rank(xL))
+xL=scales::rescale(xL,to=c(0,1))
+# Rank with ties receiving the same ranking
+xL <- rank(xL, ties.method = "average")
+# Scale ranks from 0 to 1
+xL=xL-min(xL)
+xL=xL/max(xL)
 x[,i]=xL
 }
 }
@@ -553,9 +666,13 @@ print(x)
 for (i in 1:ncol(x)){
 if (names(x)[i]!="GN"){
 xL=as.numeric(x[,i])
-xL=scales::rescale(xL,to=c(0,1))
 xL[is.na(xL)]=0
-xL=rank(xL)/max(rank(xL))
+xL=scales::rescale(xL,to=c(0,1))
+# Rank with ties receiving the same ranking
+xL <- rank(xL, ties.method = "average")
+# Scale ranks from 0 to 1
+xL=xL-min(xL)
+xL=xL/max(xL)
 x[,i]=xL
 }
 }
@@ -666,4 +783,44 @@ res[["Heatmap"]]=p1
 invisible(res)
 }
 
+
+#' Import MaxQuant protein group file
+#' @param filename_txt (string)
+#' @param data_s (string)
+#' @return DF
+#' @keywords import
+#' @examples
+#' ---
+#' @export
+ImportMaxQuantProteinGroups <- function(filename_txt,data_s="iBAQ"){
+#
+#
+DF=DFstatRM::loadFromFile(filename_txt, sep = "\t", fill = F)
+# remove contaminants and revers
+idxL=grep("^CON_",DF$Protein.IDs)
+DF=DF[-idxL,]
+idxL=grep("^REV_",DF$Protein.IDs)
+DF=DF[-idxL,]
+#import
+if (any(grepl(data_s,names(DF)))){
+print("Extracting iBAQ values")
+idxL=grep(data_s,names(DF))	
+DFt=DF[,idxL[3:length(idxL)]]
+names(DFt)=sub("iBAQ."," ",names(DFt))
+DFt$GN=ifelse(grepl(".* GN=([^ ]+).*", DF[,c("Fasta.headers")]), gsub(".* GN=([^ ]+).*", "\\1", DF[,c("Fasta.headers")]), "")
+DF=DFt
+} else {
+# grep lfq
+if (!any(grepl("LFQ.intensity.",names(DF)))){stop("No LFQ values in result file")}
+print("Extracting LFQ.intensity.")
+idxL=grep("LFQ.intensity.",names(DF))
+#names(DF)[idxL]	
+DFt=DF[,idxL]
+names(DFt)=sub("LFQ.intensity."," ",names(DFt))
+DFt$GN=ifelse(grepl(".* GN=([^ ]+).*", DF[,c("Fasta.headers")]), gsub(".* GN=([^ ]+).*", "\\1", DF[,c("Fasta.headers")]), "")
+DF=DFt
+}
+
+return(DF) # invisible(DF)
+}
 
